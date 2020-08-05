d774885a6955619f2f1149cc97ac05e2da3d20ca
andemosa <osayerieanderson@yahoo.com>
2020-08-05 12:58:06 +0100

    Require 3rd party modules (lodash)

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-console.log('Running app.js');
- 
-const fs = require('fs');
- 
-const todos = require('./todos.js');
-const _ = require('lodash');
- 
+console.log("Running app.js");
+
+const fs = require("fs");
+
+const todos = require("./todos.js");
+const _ = require("lodash");
+
 console.log(todos.addTodo());
-console.log('I love coding!');
